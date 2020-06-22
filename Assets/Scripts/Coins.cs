9283f37dd07b6505c9db2c8abb5707a81ce98a35
imBushido <maxsakamoto123@gmail.com>
2020-06-21 21:50:46 -0500

    Añadi el script de "move" al cubo y el script "Coins" a los cubitos y al esferon.

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+  
+    public float rotationSpeed;
+
+    private void Update()
+    {
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, rotationSpeed * Time.deltaTime, 0f));
+
+    }
+
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position += new Vector3(1f, 0f, 0f) * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
+            transform.position += new Vector3(-1f, 0f, 0f) * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
+            transform.position += new Vector3(0f, 0f, 1f) * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position += new Vector3(0f, 0f, -speed * Time.deltaTime);
+            transform.position += new Vector3(0f, 0f, -1f) * speed * Time.deltaTime;
+        }
+    }
+}
